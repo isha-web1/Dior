9f5c479230ba575ed330f74c9a323c05e2d8c956
isha-web1 <isha.web1@gmail.com>
2024-01-24 22:27:09 +0600

    footer added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 
+import { Outlet } from 'react-router-dom'
 import './App.css'
 import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
 
 function App() {
   
@@ -8,6 +10,8 @@ function App() {
   return (
     <>
      <Header></Header>
+     <Outlet></Outlet>
+     <Footer></Footer>
     </>
   )
 }
