f9d5b779533f90c2d436f385165446fcf2d62d2f
isha-web1 <isha.web1@gmail.com>
2024-01-24 22:19:39 +0600

    navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 
 import './App.css'
+import Header from './components/Header/Header'
 
 function App() {
   
 
   return (
     <>
-     <h1>here hter</h1>
+     <Header></Header>
     </>
   )
 }
