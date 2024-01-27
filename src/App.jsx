0bc19d08b0b01c64ce621db7ed992c005f70c9d1
isha-web1 <isha.web1@gmail.com>
2024-01-27 18:48:01 +0600

    errorpage added

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,9 @@ function App() {
   return (
     <>
      <Header></Header>
+     <div className='min-h-[calc(100vh-137px)]'>
      <Outlet></Outlet>
+     </div>
      <Footer></Footer>
     </>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -4,17 +4,28 @@ import App from './App.jsx'
 import './index.css'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import About from './components/About/About.jsx'
+import Home from './components/Home/Home.jsx'
+import ErrorPage from './components/ErrorPage.jsx'
 
 
 const router = createBrowserRouter([
  {
   path : '/',
-  element : <App></App>
+  element : <App></App>,
+  errorElement : <ErrorPage></ErrorPage>,
+  children : [
+    {
+        path : '/',
+        element : <Home></Home>
+    }
+    ,
+    {
+        path : '/about',
+        element :<About></About>
+       }
+  ]
  },
- {
-  path : '/about',
-  element :<About></About>
- }
+ 
 ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(<RouterProvider router={router}></RouterProvider>)
