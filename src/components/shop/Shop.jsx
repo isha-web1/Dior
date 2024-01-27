4833e222f3c6d2a6aa52a2ab453bde826f6c1ee0
isha-web1 <isha.web1@gmail.com>
2024-01-27 22:45:07 +0600

    add to localStorage

--- src/components/shop/Shop.jsx
+++ src/components/shop/Shop.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
 import ProductCard from '../cards/ProductsCard';
+import { addToDb } from '../../utils/FakeDb';
 
 const Shop = () => {
     const productData = useLoaderData();
-    console.log(productData)
+//    card button handler
+
+   const handleAddToCart = id =>{
+    addToDb(id)
+   }
+
     return (
         <div className='grid gap-8 md:grid-cols-2 lg:grid-cols-3'>
             {
-                productData.map((product) =><ProductCard key={product.id} product={product}></ProductCard>)
+                productData.map((product) =><ProductCard key={product.id} product={product} handleAddToCart={handleAddToCart}></ProductCard>)
             }
         </div>
     );
